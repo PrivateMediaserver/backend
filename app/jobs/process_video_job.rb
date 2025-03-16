fc62820d9d353b7999f0acfeabd2d728f5176a3e
Nikita Ignatovich <nikita@ignatovich.me>
2025-03-17 00:32:50 +0300

    change video file download for processing process

--- app/jobs/process_video_job.rb
+++ app/jobs/process_video_job.rb
@@ -3,32 +3,26 @@ class ProcessVideoJob < ApplicationJob
 
   def perform(id)
     @video = Video.find(id)
-    @video.processing!
-
     create_screenshots
   end
 
   private
 
   def create_screenshots
-    ffmpeg = Ffmpeg.new(file_path, @video.id)
-    screenshots = ffmpeg.generate_frames
+    @video.file.open do |file|
+      @video.processing!
 
-    screenshots.each_with_index do |s, i|
-      screenshot = @video.screenshots.create(main: i == 0)
-      screenshot.file.attach(io: File.open(s), filename: "#{i + 1}.png", content_type: "image/png")
+      ffmpeg = Ffmpeg.new(file.path, @video.id)
+      screenshots = ffmpeg.generate_frames
+
+      screenshots.each_with_index do |s, i|
+        screenshot = @video.screenshots.create(main: i == 0)
+        screenshot.file.attach(io: File.open(s), filename: "#{i + 1}.png", content_type: "image/png")
+      end
+
+      ffmpeg.cleanup
+
+      @video.processed!
     end
-
-    ffmpeg.cleanup
-
-    @video.processed!
-  end
-
-  def file_path
-    original_filename = @video.file.blob.filename
-    temporary_file = Tempfile.new([ original_filename.base, ".#{original_filename.extension}" ], binmode: true)
-    temporary_file.write(@video.file.download)
-
-    temporary_file.path
   end
 end
