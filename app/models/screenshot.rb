7270b2b2ffed0e81c195c4b7016e1fa4f4cf8813
Nikita Ignatovich <nikita@ignatovich.me>
2025-03-17 03:21:13 +0300

    new thumb size

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -2,7 +2,7 @@ class Screenshot < ApplicationRecord
   belongs_to :video
   has_one_attached :file do |attachable|
     attachable.variant :webp, format: :webp
-    attachable.variant :thumb, resize_to_limit: [ 320, 320 ], format: :webp
+    attachable.variant :thumb, resize_to_limit: [ 480, 480 ], format: :webp
   end
 
   validates :main, uniqueness: { scope: :video_id }, if: -> { main? }
