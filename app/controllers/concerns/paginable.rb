96c4b3ab0a7007690dca91d68f0493b8a01eac6a
Nikita Ignatovich <nikita@ignatovich.me>
2025-11-11 15:22:53 +0300

    setup new pagy options

--- app/controllers/concerns/paginable.rb
+++ app/controllers/concerns/paginable.rb
@@ -2,13 +2,13 @@ module Paginable
   extend ActiveSupport::Concern
 
   included do
-    include Pagy::Backend
+    include Pagy::Method
   end
 
   private
 
   def paginate(collection)
-    pagy(collection, page: current_page, limit: current_limit)
+    pagy(:offset, collection, page: current_page, limit: current_limit)
   end
 
   def current_page
